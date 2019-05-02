df78a6bab05d046110a9cf1bd765e0579662d080
Chalat Limpongsawatt <chalat.lim@gmail.com>
2019-05-02 10:45:39 +0700

    500ms delay

--- src/models/keyboard.js
+++ src/models/keyboard.js
@@ -1,25 +1,31 @@
 import robot from 'robotjs';
 
 export const enter = () => {
-  robot.keyTap('enter');
+  robot.keyToggle('enter', 'down');
+  setTimeout(() => robot.keyToggle('enter', 'up'), 500);
 };
 
 export const type = (word) => {
-  robot.typeString(word);
+  robot.keyToggle(word, 'down');
+  setTimeout(() => robot.keyToggle(word, 'up'), 500);
 };
 
 export const up = () => {
-  robot.keyTap('up');
+  robot.keyToggle('up', 'down');
+  setTimeout(() => robot.keyToggle('up', 'up'), 500);
 };
 
 export const down = () => {
-  robot.keyTap('down');
+  robot.keyToggle('down', 'down');
+  setTimeout(() => robot.keyToggle('down', 'up'), 500);
 };
 
 export const right = () => {
-  robot.keyTap('right');
+  robot.keyToggle('right', 'down');
+  setTimeout(() => robot.keyToggle('right', 'up'), 500);
 };
 
 export const left = () => {
-  robot.keyTap('left');
+  robot.keyToggle('left', 'down');
+  setTimeout(() => robot.keyToggle('left', 'up'), 500);
 };
